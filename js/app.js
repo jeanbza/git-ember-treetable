c3248cff1058af089d63c71744cb8c1bc9bb7459
jadekler <jadekler@gmail.com>
2013-07-31 15:49:09 -0400

    Bubblecheck was being a bit too aggressive - toned it down by letting cascades happen when all children are checked (to counterbalance incorrectly unchecking parents - now it goes back and figures out that since its children are checked, it must be checked too)

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ App.TreeBranchView = Ember.View.extend({
 });
 
 App.TreeNodeController = Ember.ObjectController.extend({
+	needs: 'treetable',
+
 	isExpanded: false,
 	checked: false,
 
@@ -18,20 +20,30 @@ App.TreeNodeController = Ember.ObjectController.extend({
 		App.TreeNodeController.degisterNodeController(this.get('content'));
 	},
 
+	toggle: function() {
+		this.set('isExpanded', !this.get('isExpanded'));
+	},
+
+	allExpandedDidChange: function() {
+		this.set('isExpanded', this.get('controllers.treetable.allExpanded'));
+	}.observes('controllers.treetable.allExpanded'),
+
 	contentDidChange: function() {
 		var node = this.get('content');
 		App.TreeNodeController.registerNodeController(node, this);
 		this.set('checked', App.get('selectedNodes').contains(node));
 	}.observes('content'),
 
-	toggle: function() {
-		this.set('isExpanded', !this.get('isExpanded'));
-	},
-
+	/**
+	 * Observer function for checked value. When checked is changed, cascade and bubble appropriately
+	 * @return void
+	 */
 	checkedDidChange: function() {
+		// We always want to bubble
 		this.bubbleChecked(this.get('content.parent'));
 
-		if(this.get('checked') == true) {
+		// We only want to cascade IF all the children are checked - otherwise our recursion will overwrite previous checking
+		if(this.get('checked') == true || this.allChildrenChecked(this.get('content'))) {
 			this.cascadeChecked(this.get('content'), this.get('checked'));
 		} else {
 			var selectedNodes = App.get('selectedNodes');
@@ -39,23 +51,26 @@ App.TreeNodeController = Ember.ObjectController.extend({
 		}
 	}.observes('checked'),
 
+	/**
+	 * Bubbles up checks where appropriate - checks the parent to see if all children are checked, in which case it checks the parent
+	 * @param  {object} node The parent
+	 * @return void
+	 */
 	bubbleChecked: function(node) {
 		var controller = App.TreeNodeController.controllerForNode(node);
 		if (!controller) {
 			return; //We've reached root
 		}
 
-		var allChecked = true;
-		Ember.get(node, 'children').forEach(function(child) {
-			var childController = App.TreeNodeController.controllerForNode(child);
-			if(childController && Ember.get(childController, 'checked') == false) {
-				allChecked = false;
-			}
-		});
-
-		controller.set('checked', allChecked);
+		controller.set('checked', this.allChildrenChecked(node));
 	},
 
+	/**
+	 * Cascades down the tree from this node, checking/unchecking and adding/removing to list of selected nodes
+	 * @param  {object} node    The parent
+	 * @param  {bool}   checked The value we're setting checked
+	 * @return void
+	 */
 	cascadeChecked: function(node, checked) {
 		var selectedNodes = App.get('selectedNodes');
 		if (checked) {
@@ -72,6 +87,22 @@ App.TreeNodeController = Ember.ObjectController.extend({
 		Ember.get(node, 'children').forEach(function(child) {
 			this.cascadeChecked(child, checked);
 		}, this);
+	},
+
+	/**
+	 * This function checks if all children of a node are checked
+	 * @param  {object} node The parent node
+	 * @return {bool}		 True/false - are the children all checked or not
+	 */
+	allChildrenChecked: function(node) {
+		var allChecked = true;
+		Ember.get(node, 'children').forEach(function(child) {
+			var childController = App.TreeNodeController.controllerForNode(child);
+			if(childController && Ember.get(childController, 'checked') == false) {
+				allChecked = false;
+			}
+		});
+		return allChecked;
 	}
 });
 App.register('controller:treeNode', App.TreeNodeController, {singleton: false});
